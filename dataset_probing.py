10691b4c15e434684d1776ab69b193a0b0b72762
will <will2346@live.co.uk>
2022-12-07 20:23:37 +0000

    init

--- dataset_probing.py
+++ dataset_probing.py
@@ -2,35 +2,59 @@ import minari
 import gym
 import d4rl.gym_mujoco
 import numpy as np
+import pandas as pd
 from minari.dataset import MinariDataset
 
 envs=[
     "halfcheetah-random-v0",
     "hopper-random-v0",
-	"walker2d-random-v0",
-	"halfcheetah-medium-v0",
-	"hopper-medium-v0",
-	"walker2d-medium-v0",
-	"halfcheetah-expert-v0",
-	"hopper-expert-v0",
-	"walker2d-expert-v0",
-	"halfcheetah-medium-expert-v0",
-	"hopper-medium-expert-v0",
-	"walker2d-medium-expert-v0",
-	"halfcheetah-medium-replay-v0",
-	"hopper-medium-replay-v0",
-	"walker2d-medium-replay-v0",
+    "walker2d-random-v0",
+    "halfcheetah-medium-v0",
+    "hopper-medium-v0",
+    "walker2d-medium-v0",
+    "halfcheetah-expert-v0",
+    "hopper-expert-v0",
+    "walker2d-expert-v0",
+    "halfcheetah-medium-expert-v0",
+    "hopper-medium-expert-v0",
+    "walker2d-medium-expert-v0",
+    "halfcheetah-medium-replay-v0",
+    "hopper-medium-replay-v0",
+    "walker2d-medium-replay-v0",
+    "halfcheetah-random-v2",
+    "hopper-random-v2",
+    "walker2d-random-v2",
+    "halfcheetah-medium-v2",
+    "hopper-medium-v2",
+    "walker2d-medium-v2",
+    "halfcheetah-expert-v2",
+    "hopper-expert-v2",
+    "walker2d-expert-v2",
+    "halfcheetah-medium-expert-v2",
+    "hopper-medium-expert-v2",
+    "walker2d-medium-expert-v2",
+    "halfcheetah-medium-replay-v2",
+    "hopper-medium-replay-v2",
+    "walker2d-medium-replay-v2",
     ]
+
+df = pd.DataFrame(columns=['Dataset', 'Size', 'Contradictions', 'Terminals'])
 
 for env in envs:
     # retrieve d4rl dataset from berkley servers
     dataset = d4rl.qlearning_dataset(gym.make(env))
+    dataset_size = len(dataset['observations'])-1
 
     contradictions = []
-    for i in range(len(dataset['observations'])-1):
+    for i in range(dataset_size):
         if not np.equal(dataset['observations'][i+1], dataset['next_observations'][i]).all():
             contradictions.append(i)
+
+    print(f"***************{env}**************")
 
     print(f"There are {len(contradictions)} instances where the observations do not match the next_observations.")
 
     print(f"There are {dataset['terminals'].sum()} terminal states.")
+    df = df.append({'Dataset': env, 'Size': dataset_size, 'Contradictions': len(contradictions), 'Terminals': dataset['terminals'].sum()}, ignore_index=True)
+
+print(df)
--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import numpy as np
 from minari.dataset import MinariDataset
 
 envs=[
-    # "halfcheetah-random-v0",
+    "halfcheetah-random-v0",
     "hopper-random-v0",
-#	"walker2d-random-v0"
-#	"halfcheetah-medium-v0"
-#	"hopper-medium-v0"
-#	"walker2d-medium-v0"
-#	"halfcheetah-expert-v0"
-#	"hopper-expert-v0"
-#	"walker2d-expert-v0"
-#	"halfcheetah-medium-expert-v0"
-#	"hopper-medium-expert-v0"
-#	"walker2d-medium-expert-v0"
-#	"halfcheetah-medium-replay-v0"
-#	"hopper-medium-replay-v0"
-#	"walker2d-medium-replay-v0"
+	"walker2d-random-v0"
+	"halfcheetah-medium-v0"
+	"hopper-medium-v0"
+	"walker2d-medium-v0"
+	"halfcheetah-expert-v0"
+	"hopper-expert-v0"
+	"walker2d-expert-v0"
+	"halfcheetah-medium-expert-v0"
+	"hopper-medium-expert-v0"
+	"walker2d-medium-expert-v0"
+	"halfcheetah-medium-replay-v0"
+	"hopper-medium-replay-v0"
+	"walker2d-medium-replay-v0"
     ]
 
 for env in envs:
@@ -41,20 +41,6 @@ for env in envs:
         rewards=np.append(env["rewards"], 0),  # env["rewards"],
         terminations=np.append(env["terminals"], 1),  # env["terminals"],  assume last transition is terminal
         truncations=np.append(env["terminals"], 1)  # env["terminals"],
-    )
-
-    dataset_two = MinariDataset(
-        dataset_name=dataset_name,
-        algorithm_name="SAC (check code permalink for details)",
-        seed_used=np.nan,
-        code_permalink="https://github.com/Farama-Foundation/D4RL/wiki/Dataset-Reproducibility-Guide",
-        author="Justin Fu",
-        author_email="justinjfu@eecs.berkeley.edu",
-        observations=env["observations"],
-        actions=env["actions"],
-        rewards=env["rewards"],
-        terminations=env["terminals"],
-        truncations=env["terminals"],
     )
 
     assert (dataset.observations[:-1] == env['observations']).all()
